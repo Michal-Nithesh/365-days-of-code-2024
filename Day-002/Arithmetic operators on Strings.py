def194661d4d3c4cf5d4a746f68ab0a58223d989
Michal Nithesh <151201789+Nithesh14@users.noreply.github.com>
2024-01-07 09:58:22 -0800

    Arithmetic operators on Strings.py

--- Day-002/Arithmetic operators on Strings.py
+++ Day-002/Arithmetic operators on Strings.py
@@ -1,1 +1,11 @@
+def main():
+    # YOUR CODE GOES HERE
+    # Please take input and print output to standard input/output (stdin/stdout)
+    # E.g. 'input()/raw_input()' for input & 'print' for output
+    my_string = "InterviewBit"
+    print(my_string * 100)
 
+    return 0
+
+if __name__ == '__main__':
+    main()
