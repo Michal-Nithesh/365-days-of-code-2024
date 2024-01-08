ecd61640b0543beee5a68ee35e066d2371539e8b
Michal Nithesh <151201789+Nithesh14@users.noreply.github.com>
2024-01-08 04:52:23 -0800

    Tuples.py

---
+++ Day-007/Tuples.py
@@ -0,0 +1,15 @@
+def main():
+    tuple1 = tuple(("number", "two", "three"))
+    tuple2 = tuple(("number", "2", "3"))
+    
+    # change value at index 0 of both tuple to string "number"
+    # Your code goes here
+    
+    
+    print(tuple1)
+    print(tuple2)
+    
+    return 0
+
+if __name__ == '__main__':
+    main()
