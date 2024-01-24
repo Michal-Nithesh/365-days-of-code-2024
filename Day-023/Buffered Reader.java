479f22a694227fd16629ad1747fa21acb3ffa8e2
Michal Nithesh <151201789+Nithesh14@users.noreply.github.com>
2024-01-24 04:09:00 -0800

    Buffered Reader.java

---
+++ Day-023/Buffered Reader.java
@@ -0,0 +1,15 @@
+import java.lang.*;
+import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new  InputStreamReader(System.in));
+        int a = Integer.parseInt(reader.readLine());
+        int b = Integer.parseInt(reader.readLine());
+        System.out.println(a + " " + b);
+        
+        //your code goes here
+    }
+}
