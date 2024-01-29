f4053c6f11dae87e91a25d8ffa2fe048861b34d8
Michal Nithesh <151201789+Nithesh14@users.noreply.github.com>
2024-01-29 05:27:36 -0800

    Loops.java

---
+++ Day-028/Loops.java
@@ -0,0 +1,17 @@
+import java.lang.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        /***Don't change the code here***/
+        Scanner inp = new Scanner(System.in);
+        int n = inp.nextInt();
+        inp.close();
+        /***********************************/       
+        /***Your code goes here***/
+        for(int i = 0; i < n; i++){
+            System.out.println(i+1);
+        }   
+        
+    }
+}
