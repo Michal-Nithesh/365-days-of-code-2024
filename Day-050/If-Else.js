3ee02dcb4e55c132bf54b360c0d100b724680b15
Michal Nithesh <151201789+Nithesh14@users.noreply.github.com>
2024-02-20 17:40:51 +0530

    Create If-Else.js

---
+++ Day-050/If-Else.js
@@ -0,0 +1,22 @@
+// DO NOT USE ARGUMENTS FOR INPUTS
+// Input num valus is in variable *num*
+var num = readLine().trim();
+// YOUR CODE GOES HERE
+if(num >= 90){
+    console.log("A")
+}
+else if(num >= 80){
+    console.log("B")
+}
+else if(num >= 70){
+    console.log("C")
+}
+else if(num >= 60){
+    console.log("D")
+}
+else if(num >= 50){
+    console.log("E")
+}
+else{
+    console.log("F")
+}
