508d2d87b48592122e55d9198f94a7aea3ae1f71
Michal Nithesh <151201789+Nithesh14@users.noreply.github.com>
2024-02-21 19:02:46 +0530

    Create SwitchStatement.js

---
+++ Day-051/SwitchStatement.js
@@ -0,0 +1,27 @@
+// DO NOT USE ARGUMENTS FOR INPUTS
+// Input num valus is in variable *num*
+var num = parseInt(readLine().trim());
+// YOUR CODE GOES HERE
+switch (num){
+    case 1:
+    console.log("Monday");
+    break;
+    case 2:
+    console.log("Tuesday");
+    break;
+    case 3:
+    console.log("Wednesday");
+    break;
+    case 4:
+    console.log("Thursday");
+    break;
+    case 5:
+    console.log("Friday");
+    break;
+    case 6:
+    console.log("Saturday");
+    break;
+    case 7:
+    console.log("Sunday");
+    break;
+}
