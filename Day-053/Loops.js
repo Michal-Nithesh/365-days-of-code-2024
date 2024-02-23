287db42f889cd686dc5f38725c8714078c668c7a
Michal Nithesh <151201789+Nithesh14@users.noreply.github.com>
2024-02-23 18:37:00 +0530

    Create Loops.js

---
+++ Day-053/Loops.js
@@ -0,0 +1,32 @@
+// DO NOT USE ARGUMENTS FOR INPUTS
+// Input num valus is in variable *num*
+var num = parseInt(readLine().trim());
+// YOUR CODE GOES HERE
+var i = 0
+
+while(i < num){
+    if(i % 2 != 0){
+        console.log(i);
+    }
+    i++;
+}
+
+//for loop
+//The syntax of for-loop is:
+
+//for (initialization; condition; update) {
+    // body of-loop 
+//}
+
+//While loop
+//The syntax of while loop is:
+
+//while (condition) {
+  //  statememt(s);
+//}
+
+//The syntax of do...while loop is:
+
+//do {
+  //body
+//} while (condition);
