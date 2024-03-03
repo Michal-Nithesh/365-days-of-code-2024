48af0451551d59f8c5f9c61866fad432f54d796d
Michal Nithesh <151201789+Nithesh14@users.noreply.github.com>
2024-03-03 18:48:16 +0530

    Create Generics.java

---
+++ Day-062/Generics.java
@@ -0,0 +1,36 @@
+import java.lang.*;
+import java.util.*;
+import java.io.IOException;
+import java.lang.reflect.Method;
+
+class Printer
+{
+   //Write your code here
+   public static < E > void printArray(E[] elements) {  
+        for ( E element : elements){          
+            System.out.println(element );  
+         }  
+    }  
+}
+
+/*****Don't change anything in the code below*****/
+public class Main {
+    public static void main(String[] args) {
+        Printer myPrinter = new Printer();
+        Integer[] intArray = { 1, 2, 3 };
+        String[] stringArray = {"Hello", "World"};
+        myPrinter.printArray(intArray);
+        myPrinter.printArray(stringArray);
+        int count = 0;
+
+        for (Method method : Printer.class.getDeclaredMethods()) {
+            String name = method.getName();
+
+            if(name.equals("printArray"))
+                count++;
+        }
+
+        if(count > 1)System.out.println("Method overloading is not allowed!");
+        
+    }
+}
