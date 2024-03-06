0deb17b58d27d9665f5ff924fa5fb242d0065b03
Michal Nithesh <151201789+Nithesh14@users.noreply.github.com>
2024-03-06 17:36:08 +0530

    Create RandomNumbers.java

---
+++ Day-065/RandomNumbers.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class myClass
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+                Random rand = new Random();
+                System.out.println(rand.nextInt(1));
+	}
+}
