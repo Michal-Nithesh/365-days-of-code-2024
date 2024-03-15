844471a7eafd7e95da2f1b7449f41b117aa2ea20
Michal Nithesh <151201789+Nithesh14@users.noreply.github.com>
2024-03-15 22:27:58 +0530

    Create ClassesAndInstances.js

---
+++ Day-74/ClassesAndInstances.js
@@ -0,0 +1,16 @@
+// Please take input and print output to standard input/output (stdin/stdout)
+// DO NOT USE ARGUMENTS FOR INPUTS
+
+var defaultName = readLine();  
+var defaultPosition = readLine();
+var defaultSkill = readLine();
+
+class Employee{
+// define the constructor function here
+    constructor() {
+        this.name = defaultName;
+        this.position = defaultPosition;
+        this.skill = defaultSkill;
+    }
+
+}
