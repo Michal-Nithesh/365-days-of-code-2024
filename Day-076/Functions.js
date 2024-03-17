22b44eb35a99d75f1d97c2f427d0eed4089f43b5
Michal Nithesh <151201789+Nithesh14@users.noreply.github.com>
2024-03-17 22:21:05 +0530

    Create Functions.js

---
+++ Day-076/Functions.js
@@ -0,0 +1,15 @@
+// DO NOT USE ARGUMENTS FOR INPUTS
+// You have to just implement the myFunction.
+
+var a = parseInt(readLine().trim());
+var b = parseInt(readLine().trim());
+var c = parseInt(readLine().trim());
+
+// YOUR CODE GOES HERE
+function myFunction(a,b,c){
+    return a*b*c;
+}
+/*
+var z = myFunction(a,b,c);
+console.log(z);
+*/
