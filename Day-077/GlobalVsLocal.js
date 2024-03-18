8b3904541c0a3b9eb86ad57059469bd3ff0745e4
Michal Nithesh <151201789+Nithesh14@users.noreply.github.com>
2024-03-18 18:56:05 +0530

    Create GlobalVsLocal.js

---
+++ Day-077/GlobalVsLocal.js
@@ -0,0 +1,22 @@
+// Local Scope
+function myFunction() {
+   var stud = "Adarsh";
+  // Here we can use stud variable.
+}
+
+function myFunction1() {
+   var stud = "Adarsh";
+  // Rest Code.
+}
+
+function myFunction1() {
+   var stud = "Vicky";
+  // Rest Code.
+}
+
+// Global Scpoe
+var stud = "Adarsh";
+ // Here we can use stud variable.
+function myFunction() {
+  // Here we can also use stud variable.
+}
