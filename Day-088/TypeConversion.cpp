4423a03399122a5da7457a00acb0ac2f7e4d0a5f
Michal Nithesh <151201789+Nithesh14@users.noreply.github.com>
2024-03-29 21:49:28 +0530

    Create TypeConversion.cpp

---
+++ Day-088/TypeConversion.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int main()  {
+    char ch;
+    int a;
+    cin>>ch;
+    a = int(ch);
+    //Your code goes here
+    cout << a << endl;
+    return 0;
+}
