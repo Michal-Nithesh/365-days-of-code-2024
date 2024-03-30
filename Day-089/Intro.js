519e2f8bb76658715193b907d4978995040d2fed
Michal Nithesh <151201789+Nithesh14@users.noreply.github.com>
2024-03-30 23:07:57 +0530

    Create Intro.js

---
+++ Day-089/Intro.js
@@ -0,0 +1,11 @@
+let js ={
+         name : "JavaScript",
+         abbreviations : "JS",
+         isAwesome : true,
+         officialSpec : "ECMAScript",
+         birthYear : 1995,
+         creator : "Brendan Eich",
+};
+if (js.isAwesome){
+           shout("Hell yes JS!!")
+}
