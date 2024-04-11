634fbd5b097bdfad30fc81da2fea30adb4d9138a
Michal Nithesh <151201789+Nithesh14@users.noreply.github.com>
2024-04-11 18:01:41 +0530

    Create Functions-Basic.cpp

---
+++ Day-100/Functions-Basic.cpp
@@ -0,0 +1,18 @@
+/*#include<iostream>
+using namespace std;
+*/
+
+// Your Code goes here
+int compute (int A , int B)
+{
+    return (A*A + B*B);
+}
+
+/*
+int main()  {
+    int A, B;
+    cin >> A >>B;
+    cout<<compute(A, B)<<endl;
+    return 0;
+}
+*/
