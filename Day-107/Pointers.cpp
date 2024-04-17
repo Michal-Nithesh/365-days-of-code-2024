aa087f2187e71c89f85e6894007b7c955505d31e
Michal Nithesh <151201789+Nithesh14@users.noreply.github.com>
2024-04-17 22:42:59 +0530

    Create Pointers.cpp

---
+++ Day-107/Pointers.cpp
@@ -0,0 +1,29 @@
+/*
+#include<iostream>
+using namespace std;
+*/
+
+void solve(int *A, int *B){
+    int temp = *A;
+    *A = *A +*B;
+    *B = temp - *B;
+    
+    if(*B < 0)
+    {
+        *B = -*B;
+    }
+}
+
+
+
+/*
+int main()  {
+    int A, B;
+    int *pA = &A, *pB = &B;
+    cin>>A>>B;
+    solve(pA, pB);
+    cout<<A<<endl;
+    cout<<B<<endl;
+    return 0;
+}
+*/
