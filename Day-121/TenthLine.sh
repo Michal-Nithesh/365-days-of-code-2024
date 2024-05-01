a061e74d29d38e2f5e72c92ad1bd994b86d86f09
Michal Nithesh <151201789+Nithesh14@users.noreply.github.com>
2024-05-01 21:55:57 +0530

    Create TenthLine.sh

---
+++ Day-121/TenthLine.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Incase you have to take input, please take it from file named 'input' (without quotes) [e.g. cat input]
+# Print your final output to console. Do not redirect to another file.
+# E.g. Suppose the question is to print the content of a file.
+#      Your solution should be 'cat input'(without quotes) instead of 'cat input > output'. That's it!
+# Your code starts from here...
+cat input | head -n 10 | tail -n 1
