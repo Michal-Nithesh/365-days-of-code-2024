4cd49512a121b151982306c2f017faac51f792d9
Michal Nithesh <151201789+Nithesh14@users.noreply.github.com>
2024-05-11 23:48:37 +0530

    Create REC_CMPL1.c

---
+++ Day-131/REC_CMPL1.c
@@ -0,0 +1,12 @@
+/* 
+ * V is sorted 
+ * V.size() = N
+ * The function is initially called as searchNumOccurrence(V, k, 0, N-1)
+ */
+int searchNumOccurrence(vector<int> &V, int k, int start, int end) {
+    if (start > end) return 0;
+    int mid = (start + end) / 2;
+    if (V[mid] < k) return searchNumOccurrence(V, k, mid + 1, end);
+    if (V[mid] > k) return searchNumOccurrence(V, k, start, mid - 1);
+    return searchNumOccurrence(V, k, start, mid - 1) + 1 + searchNumOccurrence(V, k, mid + 1, end);
+}
