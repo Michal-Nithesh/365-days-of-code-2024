84782fdc7317561a0ca22fae80d776c2ac01854a
Michal Nithesh <151201789+Nithesh14@users.noreply.github.com>
2024-06-02 22:06:11 +0530

    Create Word Count.py

---
+++ Day-153/Word Count.py
@@ -0,0 +1,17 @@
+class Solution:
+    # @param A : string
+    # @return an integer
+    def solve(self, A):
+        A=A.split(" ")
+
+        c=len(A)
+
+        for i in A:
+
+            if(i==""):
+
+                c-=1
+
+            
+
+        return c
