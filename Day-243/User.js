cbd706c86180cb208e5ef8197ef6e371c62ffd6f
Michal Nithesh <151201789+Michal-Nithesh@users.noreply.github.com>
2024-08-31 22:26:09 +0530

    Create User.js

---
+++ Day-243/User.js
@@ -0,0 +1,17 @@
+let users =[
+    {
+    name: "Calvin",
+    surname: "Hart",
+    age: 66,
+    email: "CalvinMHart@teleworm.us"
+    },
+    {
+    name: "Mateus",
+    surname: "Pinto",
+    age: 21,
+    email: "MateusPinto@dayrep.com"
+    }
+];
+   
+console.log(users[0].name); // -> Calvin
+console.log(users[1].age); // -> 21
