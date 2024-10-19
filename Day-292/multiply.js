461d14ec3e75987e44f07ba43fb9674a8f3cbc50
Michal Nithesh <151201789+Michal-Nithesh@users.noreply.github.com>
2024-10-19 23:00:09 +0530

    Create multiply.js

---
+++ Day-292/multiply.js
@@ -0,0 +1,8 @@
+const number = 3;
+function multiplyBy2(inuptNumber){
+    const result = inuptNumber * 2;
+    return result
+}
+
+const output = multiplyBy2(number);
+const newOutput = multiplyBy2(10);
